c6a0d8b0678849073678ba4349140fdd66444034
bhdzit97@gmail.com <bhdzit97@gmail.com>
2022-05-16 22:09:26 -0500

    Correcion de Corss

--- comparacionEmpleos/settings.py
+++ comparacionEmpleos/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps
     'polls.apps.PollsConfig',
+     "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+
+CORS_ALLOWED_ORIGINS =[
+    
 ]
 
 ROOT_URLCONF = 'comparacionEmpleos.urls'
